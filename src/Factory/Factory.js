cbd566e04de5cd4ce365b327d56537c31662c55f
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2018-09-20 19:01:20 +0800

    Removed flow types, should be added on a separate lib-def

--- src/Factory/Factory.js
+++ src/Factory/Factory.js
@@ -2,28 +2,16 @@
 import * as React from "react";
 import getSelectedTheme from "../utils/getSelectedTheme";
 
-function Factory<T>(
-  themeConfig: T
-): {
-  EzThemeProvider: React.ComponentType<React.ProviderProps<T>>,
-  EzThemeConsumer: React.ComponentType<React.ConsumerProps<*>>,
-  withTheme: ({ staticItems?: Object }) => React.Node => React.Node
-} {
-  const { Provider, Consumer } = React.createContext<T>(themeConfig);
+function Factory(themeConfig) {
+  const { Provider, Consumer } = React.createContext(themeConfig);
 
-  const ThemeProvider = ({
-    name,
-    children
-  }: {
-    name: string,
-    children: any
-  }) => {
+  const ThemeProvider = ({ name, children }) => {
     const selectedTheme = getSelectedTheme(name, themeConfig);
 
     return <Provider value={selectedTheme}>{children}</Provider>;
   };
 
-  const withTheme = (staticItems: Object = {}): React.Node => Component => {
+  const withTheme = (staticItems = {}) => Component => {
     const WrappedComponent = props => (
       <Consumer>{theme => <Component {...props} theme={theme} />}</Consumer>
     );
